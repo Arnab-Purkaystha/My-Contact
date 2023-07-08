54825168033ceb1c7f7cf3174625d20d0cee1bac
ARNAB PURKAYSTHA <arnab10904@gmail.com>
2023-07-09 02:05:33 +0530

    Upto databasse

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,9 +1,12 @@
-const asyncHandler = require("express-async-handler") // we dont have to write each try-catch block for the functions as async require each try-catch block
+const asyncHandler = require("express-async-handler"); // we dont have to write each try-catch block for the functions as async require each try-catch block
+const Contact  = require("../models/contactModel")
+
 //desc Get all contacts
 //get /api/contacts
 //access public
 const getContacts = asyncHandler(async (req, res) =>{
-    res.send("Get all contacts")
+    const contacts = await Contact.find();
+    res.status(201).json(contacts)
 });
 
 //desc Create new contact
@@ -12,11 +15,16 @@ const getContacts = asyncHandler(async (req, res) =>{
 const createContact = asyncHandler(async (req, res) =>{
     console.log("the request body is :" , req.body)
     const {name, email, phone } = req.body;
-    if(!name || ! email || !phone){
+    if(!name || !email || !phone){
         res.status(400);
         throw new Error("All fields are mandatory!!")
     }
-    res.status(201).json({message:"Create  contact"})
+    const contact  =await Contact.create({
+        name,
+        email,
+        phone,
+    });
+    res.status(201).json(contact)
 
 });
 
@@ -24,19 +32,40 @@ const createContact = asyncHandler(async (req, res) =>{
 //get /api/contacts
 //access public
 const getContact = asyncHandler(async (req, res) =>{
-    res.send(`Get Contact for ${req.params.id}`);
+    const contact = await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404);
+        throw new error("Contact not found");
+    }
+    res.status(201).json(contact)
 });
 
 //desc Update  contact
 //put /api/contacts
 //access public
 const updateContact = asyncHandler(async (req, res) =>{
-    res.send(`Update Contact for ${req.params.id}`);
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new error("Contact not found");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new : true}
+    );
+    res.status(201).json(updatedContact)
 });
 //desc delete  contact
 //delete /api/contacts
 //access public
-const deleteContact =asyncHandler(async (req, res) =>{
-    res.send(`Delete Contact for ${req.params.id}`);
+const deleteContact = asyncHandler(async (req, res) =>{
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    await Contact.deleteOne({ _id: req.params.id });
+  res.status(200).json(contact);
 });
 module.exports = {getContacts, getContact, createContact, updateContact, deleteContact};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,19 +2,14 @@ const express  =require("express");
 const router = express.Router();
 const {getContacts, getContact, createContact, updateContact, deleteContact} = require("../controllers/contactController")
 
-router.route("/").get(getContacts);
+
 
 // router.route("/").post((req, res) =>{
 //     res.send("Create  contact")
 // });  if we dont use controllers we have to do it manually
-router.route("/").post(createContact)
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-router.route("/:id").get(getContact);
-
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
 
 
 
