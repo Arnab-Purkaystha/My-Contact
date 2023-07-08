2b3b690062529c8d907acad6f9bda16c555b9dfb
ARNAB PURKAYSTHA <arnab10904@gmail.com>
2023-07-09 03:03:23 +0530

    user log in

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,19 @@ const registerUser = asyncHandler(async (req, res) =>{
   //Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log("Hashed Password: ", hashedPassword);
+  const user  =await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  })
+  console.log(`User Created ${user}`);
+  if(user) {
+    res.status(201).json({ _id: user.id,email: user.email});
+  }
+  else{
+    res.status(400);
+    throw new Error("User data is not valid")
+  }
     res.json({Message : "Register the user"});
 });
 
